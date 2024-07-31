789f27237f3f164b7994d01527cfe2895cd3c393
ARLJohnston <40921666+ARLJohnston@users.noreply.github.com>
2024-07-31 14:00:50 +0100

    feat: add templ

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Go WASM dependencies flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+  (flake-utils.lib.eachDefaultSystem
+  (system:
+  let
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in
+  {
+    devShell = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        go-tools
+        go_1_22
+        gopls
+        gotools
+        nixpkgs-fmt
+        nil
+        templ
+      ];
+    };
+  })
+  );
+}
